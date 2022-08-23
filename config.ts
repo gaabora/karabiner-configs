e5b216cb97631ef746808f0cbc5277f9b3e820de
FlowGurus <101252355+FlowGurus@users.noreply.github.com>
2022-08-23 13:58:07 +0300

    start migration to ts+deno

---
+++ config.ts
@@ -0,0 +1,196 @@
+import { Condition, KarabinerComplexModifications, Key, KeyPressFrom, KeyPressTo, Manipulator } from './lib/karabiner.ts';
+
+// import { writeHyperKeyImage } from 'https://deno.land/x/karabiner@v0.1.1/svg.ts';
+// import {
+//     HyperKey,
+//     KarabinerComplexModifications,
+// } from 'https://deno.land/x/karabiner@v0.1.1/karabiner.ts';
+
+
+const CHAR_keys = [
+    '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
+    'hyphen',
+    'equal_sign',
+    'grave_accent_and_tilde',
+    'comma',
+    'period',
+    'slash',
+    'spacebar',
+    'non_us_backslash',
+    'open_bracket',
+    'close_bracket',
+    'backslash',
+    'semicolon',
+    'quote',
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+] as Key[]
+const FN_keys = ['f1', 'f2', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8', 'f9', 'f10','f11', 'f12' ] as Key[]
+
+
+const AppleKeyboard_identifiers = [{ 'vendor_id': 1452 }];
+
+const RDP_or_VM_bundle_identifiers = [
+    '^com.microsoft.rdc$',
+    '^com.microsoft.rdc.mac$',
+    '^com.microsoft.rdc.osx.beta$',
+    '^com.vmware.fusion$',
+    '^com.vmware.horizon$',
+    '^com.vmware.view$',
+    '^com.parallels.desktop$',
+    '^com.parallels.vm$',
+    '^com.parallels.desktop.console$',
+    '^org.virtualbox.app.VirtualBoxVM$',
+    '^com.vmware.proxyApp.',
+    '^com.parallels.winapp.',
+    '^com.2X.Client.Mac$'
+];
+
+const overMacApps = { type: 'frontmost_application_unless', bundle_identifiers: RDP_or_VM_bundle_identifiers } as Condition;
+const overWinApps = { type: 'frontmost_application_if', bundle_identifiers: RDP_or_VM_bundle_identifiers } as Condition;
+const usingMacKBs = { type: 'device_if', identifiers: AppleKeyboard_identifiers } as Condition;
+const usingWinKBs = { type: 'device_unless', identifiers: AppleKeyboard_identifiers } as Condition;
+
+function genHotkeyTo(from: KeyPressFrom, to: KeyPressTo, conditions = [] as Condition[]): Manipulator {
+    return { type: 'basic', from: from, to: [ to ], conditions: conditions }
+}
+
+const complexMods = new KarabinerComplexModifications();
+
+// [win-like] apple kb mac app (Fn>LCmd, LCtrl>Fn, extra)
+// [win-like] apple kb win app (Fn>LCtrl, LCtrl>Fn, LAlt>Win, LCmd>LAlt, RCmd>RAlt, RAlt>RCtrl)
+// [win-like] us kb mac app (lCtrl>rCmd, extra)
+// [macbook] RAlt Extra
+//  ← ↑ → ↓
+
+complexMods.addRule({
+    description: '[win-like] apple kb (LCmd>Fn, Fn>LCtrl, extra) on VM/RDP (LCtrl><Fn, LAlt><LCmd, RCmd>RAlt, RAlt>RCtrl)',
+    manipulators: (() => {
+        const mList = [] as Manipulator[];
+
+        // REMAP FOR CHAR_keys
+        CHAR_keys.forEach(keyCode => {
+            // --- MAC KEYBOARD (fn ctrl alt cmd space cmd alt)
+            // Lcmd TO fn (only over win apps)
+            mList.push( genHotkeyTo({ key_code: keyCode, modifiers: { mandatory: ['fn'], optional: ['any'] } }, { key_code: keyCode, modifiers: [ 'left_command' ] }, [ usingMacKBs, overMacApps ]) );
+            // Lctrl TO fn (only over win apps)
+            mList.push( genHotkeyTo({ key_code: keyCode, modifiers: { mandatory: ['fn'], optional: ['any'] } }, { key_code: keyCode, modifiers: [ 'left_control' ] }, [ usingMacKBs, overWinApps ]) );
+            
+            // --- WIN KEYBOARD (ctrl [fn] win alt space alt [win] ctrl)
+            // cmd TO ctrl (mac apps)
+            mList.push( genHotkeyTo({ key_code: keyCode, modifiers: { mandatory: ['left_control'], optional: ['any'] } }, { key_code: keyCode, modifiers: [ 'left_command' ] }, [ usingWinKBs, overMacApps ]) );
+            // usually nothing needed (win apps)
+            // ...
+        });
+
+        // FOR F1-F12
+        // fn TO LCtrl
+        mList.push( genHotkeyTo({ key_code: 'f1',  modifiers: { mandatory: ['left_control'] } }, { key_code: 'display_brightness_decrement' }, [ usingMacKBs ]) );
+        mList.push( genHotkeyTo({ key_code: 'f2',  modifiers: { mandatory: ['left_control'] } }, { key_code: 'display_brightness_increment' }, [ usingMacKBs ]) );
+        mList.push( genHotkeyTo({ key_code: 'f3',  modifiers: { mandatory: ['left_control'] } }, { key_code: 'mission_control' },              [ usingMacKBs ]) );
+        mList.push( genHotkeyTo({ key_code: 'f4',  modifiers: { mandatory: ['left_control'] } }, { key_code: 'launchpad' },                    [ usingMacKBs ]) );
+        mList.push( genHotkeyTo({ key_code: 'f5',  modifiers: { mandatory: ['left_control'] } }, { key_code: 'illumination_decrement' },       [ usingMacKBs ]) );
+        mList.push( genHotkeyTo({ key_code: 'f6',  modifiers: { mandatory: ['left_control'] } }, { key_code: 'illumination_increment' },       [ usingMacKBs ]) );
+        mList.push( genHotkeyTo({ key_code: 'f7',  modifiers: { mandatory: ['left_control'] } }, { key_code: 'rewind' },                       [ usingMacKBs ]) );
+        mList.push( genHotkeyTo({ key_code: 'f8',  modifiers: { mandatory: ['left_control'] } }, { key_code: 'play_or_pause' },                [ usingMacKBs ]) );
+        mList.push( genHotkeyTo({ key_code: 'f9',  modifiers: { mandatory: ['left_control'] } }, { key_code: 'fastforward' },                  [ usingMacKBs ]) );
+        mList.push( genHotkeyTo({ key_code: 'f10', modifiers: { mandatory: ['left_control'] } }, { key_code: 'mute' },                         [ usingMacKBs ]) );
+        mList.push( genHotkeyTo({ key_code: 'f11', modifiers: { mandatory: ['left_control'] } }, { key_code: 'volume_decrement' },             [ usingMacKBs ]) );
+        mList.push( genHotkeyTo({ key_code: 'f12', modifiers: { mandatory: ['left_control'] } }, { key_code: 'volume_increment' },             [ usingMacKBs ]) );
+        // fn TO RAlt 
+        mList.push( genHotkeyTo({ key_code: 'f7',  modifiers: { mandatory: ['right_alt'] } }, { key_code: 'rewind' },           [ usingMacKBs ]) );
+        mList.push( genHotkeyTo({ key_code: 'f8',  modifiers: { mandatory: ['right_alt'] } }, { key_code: 'play_or_pause' },    [ usingMacKBs ]) );
+        mList.push( genHotkeyTo({ key_code: 'f9',  modifiers: { mandatory: ['right_alt'] } }, { key_code: 'fastforward' },      [ usingMacKBs ]) );
+        mList.push( genHotkeyTo({ key_code: 'f10', modifiers: { mandatory: ['right_alt'] } }, { key_code: 'mute' },             [ usingMacKBs ]) );
+        mList.push( genHotkeyTo({ key_code: 'f11', modifiers: { mandatory: ['right_alt'] } }, { key_code: 'volume_decrement' }, [ usingMacKBs ]) );
+        mList.push( genHotkeyTo({ key_code: 'f12', modifiers: { mandatory: ['right_alt'] } }, { key_code: 'volume_increment' }, [ usingMacKBs ]) );
+        // LCtrl to fn
+        FN_keys.forEach(keyCode => {
+            mList.push( genHotkeyTo({ key_code: keyCode, modifiers: { mandatory: ['fn'], optional: ['any'] } }, { key_code: keyCode, modifiers: [ 'left_control' ] }, [ usingMacKBs, overMacApps ]) )
+        })
+
+        // win-like ctrl + ← → behaviour on fn + ← →
+        mList.push( genHotkeyTo({ key_code: 'left_arrow',  modifiers: { mandatory: ['fn'],           optional: ['left_shift'] } }, { key_code: 'left_arrow',  modifiers: [ 'left_alt' ] }, [ usingMacKBs, overMacApps ]) )
+        mList.push( genHotkeyTo({ key_code: 'right_arrow', modifiers: { mandatory: ['fn'],           optional: ['left_shift'] } }, { key_code: 'right_arrow', modifiers: [ 'left_alt' ] }, [ usingMacKBs, overMacApps ]) )
+        mList.push( genHotkeyTo({ key_code: 'left_arrow',  modifiers: { mandatory: ['left_control'], optional: ['left_shift'] } }, { key_code: 'left_arrow',  modifiers: [ 'left_alt' ] }, [ usingWinKBs, overMacApps ]) )
+        mList.push( genHotkeyTo({ key_code: 'right_arrow', modifiers: { mandatory: ['left_control'], optional: ['left_shift'] } }, { key_code: 'right_arrow', modifiers: [ 'left_alt' ] }, [ usingWinKBs, overMacApps ]) )
+        
+        // ctrl + tab TO fn + tab
+        mList.push( genHotkeyTo({ key_code: 'tab', modifiers: { mandatory: ['fn'], optional: ['left_shift'] } }, { key_code: 'tab', modifiers: [ 'left_control' ] }, [ usingMacKBs, overMacApps ]) )
+        
+        // RDP, Virtual Machines (alt > win, cmd > alt) and fn <> ctrl for apple keyboards
+        mList.push( genHotkeyTo({ key_code: 'fn',            modifiers: { optional: ['any'] } }, { key_code: 'left_control'  }, [ usingMacKBs, overWinApps ]) )
+        mList.push( genHotkeyTo({ key_code: 'left_control',  modifiers: { optional: ['any'] } }, { key_code: 'fn'            }, [ usingMacKBs, overWinApps ]) )
+        mList.push( genHotkeyTo({ key_code: 'left_alt',      modifiers: { optional: ['any'] } }, { key_code: 'left_command'  }, [ usingMacKBs, overWinApps ]) )
+        mList.push( genHotkeyTo({ key_code: 'left_command',  modifiers: { optional: ['any'] } }, { key_code: 'left_alt'      }, [ usingMacKBs, overWinApps ]) )
+        mList.push( genHotkeyTo({ key_code: 'right_command', modifiers: { optional: ['any'] } }, { key_code: 'right_alt'     }, [ usingMacKBs, overWinApps ]) )
+        mList.push( genHotkeyTo({ key_code: 'right_alt',     modifiers: { optional: ['any'] } }, { key_code: 'right_control' }, [ usingMacKBs, overWinApps ]) )
+
+
+        // [win-like] us kb (lCtrl>rCmd, extra)
+
+        // { 'type': 'basic', 'from': { 'key_code': 'right_arrow',            'modifiers': { 'mandatory': ['control'], 'optional': ['any'] } }, 'to': [ { 'key_code': 'end',               'modifiers': [ ] } ], 'conditions': [ { 'type': 'device_if', 'identifiers': [{ 'vendor_id': 1452 }] } ] },
+        // { 'type': 'basic', 'from': { 'key_code': 'left_arrow',             'modifiers': { 'mandatory': ['control'], 'optional': ['any'] } }, 'to': [ { 'key_code': 'home',              'modifiers': [ ] } ], 'conditions': [ { 'type': 'device_if', 'identifiers': [{ 'vendor_id': 1452 }] } ] },
+        // { 'type': 'basic', 'from': { 'key_code': 'up_arrow',               'modifiers': { 'mandatory': ['control'], 'optional': ['any'] } }, 'to': [ { 'key_code': 'page_up',           'modifiers': [ ] } ], 'conditions': [ { 'type': 'device_if', 'identifiers': [{ 'vendor_id': 1452 }] } ] },
+        // { 'type': 'basic', 'from': { 'key_code': 'down_arrow',             'modifiers': { 'mandatory': ['control'], 'optional': ['any'] } }, 'to': [ { 'key_code': 'page_down',         'modifiers': [ ] } ], 'conditions': [ { 'type': 'device_if', 'identifiers': [{ 'vendor_id': 1452 }] } ] },
+
+
+        return  mList
+    })()
+})
+
+complexMods.addRule({
+    description: 'RShift + Backspace = Delete (word with fn) for apple keyboard',    
+    manipulators: (() => {
+        const mList = [] as Manipulator[];
+        
+        mList.push( genHotkeyTo({ key_code: 'delete_or_backspace', modifiers: { mandatory: ['fn', 'right_shift'], optional: [ 'any' ] } }, { key_code: 'delete_forward',      modifiers: [ 'left_option' ] }, [ usingMacKBs, overMacApps ]) )
+        mList.push( genHotkeyTo({ key_code: 'delete_or_backspace', modifiers: { mandatory: ['fn', 'right_shift'], optional: [ 'any' ] } }, { key_code: 'delete_forward',      modifiers: [ 'left_control' ] },  [ usingMacKBs, overWinApps ]) )
+        mList.push( genHotkeyTo({ key_code: 'delete_or_backspace', modifiers: { mandatory: ['fn'               ], optional: [ 'any' ] } }, { key_code: 'delete_or_backspace', modifiers: [ 'left_option' ] }, [ usingMacKBs ] ) )
+        mList.push( genHotkeyTo({ key_code: 'delete_or_backspace', modifiers: { mandatory: ['right_shift'      ], optional: [ 'any' ] } }, { key_code: 'delete_forward',      modifiers: [ ] }, [ usingMacKBs ] ) )
+        
+        return mList
+    })()
+})
+
+complexMods.addRule({
+    description: 'RShift + Enter = Insert for apple keyboard',
+    manipulators: (() => {
+        const mList = [] as Manipulator[];
+
+        mList.push( genHotkeyTo({ key_code: 'return_or_enter', modifiers: { mandatory: ['right_shift'], optional: [ 'any' ] } }, { key_code: 'insert' }, [ usingMacKBs ]) )
+        
+        return mList
+    })()
+})
+
+complexMods.addRule({
+    description: `NUMPAD SIMUL: RAlt + k,./l;'op[ = keypad_0-9 [9]>[/] [0]>[*] ']'>[.]`,
+    manipulators: (() => {
+        const mList = [] as Manipulator[];
+
+        mList.push( genHotkeyTo({ key_code: 'comma',         modifiers: { mandatory: ['right_alt'] } }, { key_code: 'keypad_1'        }, [ usingMacKBs ]) )
+        mList.push( genHotkeyTo({ key_code: 'period',        modifiers: { mandatory: ['right_alt'] } }, { key_code: 'keypad_2'        }, [ usingMacKBs ]) )
+        mList.push( genHotkeyTo({ key_code: 'slash',         modifiers: { mandatory: ['right_alt'] } }, { key_code: 'keypad_3'        }, [ usingMacKBs ]) )
+        mList.push( genHotkeyTo({ key_code: 'k',             modifiers: { mandatory: ['right_alt'] } }, { key_code: 'keypad_0'        }, [ usingMacKBs ]) )
+        mList.push( genHotkeyTo({ key_code: 'l',             modifiers: { mandatory: ['right_alt'] } }, { key_code: 'keypad_4'        }, [ usingMacKBs ]) )
+        mList.push( genHotkeyTo({ key_code: 'semicolon',     modifiers: { mandatory: ['right_alt'] } }, { key_code: 'keypad_5'        }, [ usingMacKBs ]) )
+        mList.push( genHotkeyTo({ key_code: 'quote',         modifiers: { mandatory: ['right_alt'] } }, { key_code: 'keypad_6'        }, [ usingMacKBs ]) )
+        mList.push( genHotkeyTo({ key_code: 'o',             modifiers: { mandatory: ['right_alt'] } }, { key_code: 'keypad_7'        }, [ usingMacKBs ]) )
+        mList.push( genHotkeyTo({ key_code: 'p',             modifiers: { mandatory: ['right_alt'] } }, { key_code: 'keypad_8'        }, [ usingMacKBs ]) )
+        mList.push( genHotkeyTo({ key_code: '9',             modifiers: { mandatory: ['right_alt'] } }, { key_code: 'keypad_slash'    }, [ usingMacKBs ]) )
+        mList.push( genHotkeyTo({ key_code: '0',             modifiers: { mandatory: ['right_alt'] } }, { key_code: 'keypad_asterisk' }, [ usingMacKBs ]) )
+        mList.push( genHotkeyTo({ key_code: 'open_bracket',  modifiers: { mandatory: ['right_alt'] } }, { key_code: 'keypad_9'        }, [ usingMacKBs ]) )
+        mList.push( genHotkeyTo({ key_code: 'close_bracket', modifiers: { mandatory: ['right_alt'] } }, { key_code: 'keypad_period'   }, [ usingMacKBs ]) )
+        mList.push( genHotkeyTo({ key_code: 'hyphen',        modifiers: { mandatory: ['right_alt'] } }, { key_code: 'keypad_hyphen'   }, [ usingMacKBs ]) )
+        mList.push( genHotkeyTo({ key_code: 'equal_sign',    modifiers: { mandatory: ['right_alt'] } }, { key_code: 'keypad_plus'     }, [ usingMacKBs ]) )
+        
+        return mList
+    })()
+})
+
+complexMods.writeToProfile('Default profile');
+
+
+
+
+
---
+++ lib/karabiner.ts
@@ -0,0 +1,501 @@
+export type Key =
+    | "right_command"
+    | "right_control"
+    | "right_option"
+    | "right_shift"
+    | "right_alt"
+    | "left_command"
+    | "left_control"
+    | "left_option"
+    | "left_shift"
+    | "left_alt"
+    | "left_gui"
+    | "caps_lock"
+    | "return_or_enter"
+    | "spacebar"
+    | "left_arrow"
+    | "right_arrow"
+    | "up_arrow"
+    | "down_arrow"
+    | "grave_accent_and_tilde"
+    | "non_us_pound"
+    | "delete_or_backspace"
+    | "delete_forward"
+    | "non_us_backslash"
+    | "application"
+    | "semicolon"
+    | "quote"
+    | "escape"
+    | "open_bracket"
+    | "close_bracket"
+    | "slash"
+    | "backslash"
+    | "period"
+    | "comma"
+    | "equal_sign"
+    | "hyphen"
+    | "insert"
+    | "pause"
+    | "home"
+    | "end"
+    | "page_up"
+    | "page_down"
+    | "scroll_lock"
+    | "print_screen"
+    | "1"
+    | "2"
+    | "3"
+    | "4"
+    | "5"
+    | "6"
+    | "7"
+    | "8"
+    | "9"
+    | "0"
+    | "q"
+    | "w"
+    | "e"
+    | "r"
+    | "t"
+    | "y"
+    | "u"
+    | "i"
+    | "o"
+    | "p"
+    | "a"
+    | "s"
+    | "d"
+    | "f"
+    | "g"
+    | "h"
+    | "j"
+    | "k"
+    | "l"
+    | "z"
+    | "x"
+    | "c"
+    | "v"
+    | "b"
+    | "n"
+    | "m"
+    | "fn"
+    | "display_brightness_decrement"
+    | "display_brightness_increment"
+    | "mission_control"
+    | "launchpad"
+    | "illumination_decrement"
+    | "illumination_increment"
+    | "rewind"
+    | "play_or_pause"
+    | "fastforward"
+    | "mute"
+    | "volume_decrement"
+    | "volume_increment"
+    | "tab"
+    | "f1"
+    | "f2"
+    | "f3"
+    | "f4"
+    | "f5"
+    | "f6"
+    | "f7"
+    | "f8"
+    | "f9"
+    | "f10"
+    | "f11"
+    | "f12"
+    | "keypad_1"
+    | "keypad_2"
+    | "keypad_3"
+    | "keypad_0"
+    | "keypad_4"
+    | "keypad_5"
+    | "keypad_6"
+    | "keypad_7"
+    | "keypad_8"
+    | "keypad_slash"
+    | "keypad_asterisk"
+    | "keypad_9"
+    | "keypad_period"
+    | "keypad_hyphen"
+    | "keypad_plus"
+
+
+export interface KeyPressFrom {
+    key_code: Key;
+    modifiers?: {
+        mandatory?: Key[];
+        optional?: (Key | "any")[];
+    };
+}
+
+export interface KeyPressTo {
+    key_code?: Key;
+    shell_command?: string;
+    modifiers?: Key[];
+    lazy?: boolean;
+    halt?: boolean;
+    set_variable?: {
+        name: string;
+        value: number | string;
+    };
+}
+
+/**
+ * https://karabiner-elements.pqrs.org/docs/json/complex-modifications-manipulator-definition/conditions/frontmost-application/
+ */
+export interface FrontmostApplicationCondition {
+    type: "frontmost_application_if" | "frontmost_application_unless";
+    description?: string;
+    bundle_identifiers?: string[];
+    file_paths?: string[];
+}
+
+/**
+ * https://karabiner-elements.pqrs.org/docs/json/complex-modifications-manipulator-definition/conditions/device/
+ */
+export interface DeviceCondition {
+    type: "device_if" | "device_unless";
+    identifiers: {
+        vendor_id?: number;
+        product_id?: number;
+        location_id?: boolean;
+        is_keyboard?: boolean;
+        is_pointing_device?: boolean;
+    }[];
+}
+
+/**
+ * https://karabiner-elements.pqrs.org/docs/json/complex-modifications-manipulator-definition/conditions/variable/
+ */
+export interface VariableCondition {
+    type: "variable_if" | "variable_unless";
+    name: string;
+    value: number | string;
+}
+
+/**
+ * https://karabiner-elements.pqrs.org/docs/json/complex-modifications-manipulator-definition/conditions/
+ */
+export type Condition =
+    | FrontmostApplicationCondition
+    | DeviceCondition
+    | VariableCondition;
+
+export interface AltCondition {
+    disable: Condition[];
+    enable: Condition[];
+}
+
+/**
+ * https://karabiner-elements.pqrs.org/docs/json/complex-modifications-manipulator-definition/
+ */
+export interface Manipulator {
+    type: "basic";
+    from: KeyPressFrom;
+    to?: KeyPressTo[];
+    to_if_alone?: KeyPressTo[];
+    to_after_key_up?: KeyPressTo[];
+    to_if_held_down?: KeyPressTo[];
+
+    /**
+     * https://karabiner-elements.pqrs.org/docs/json/complex-modifications-manipulator-definition/to-delayed-action/
+     */
+    to_delayed_action?: {
+        to_if_invoked?: KeyPressTo[];
+        to_if_canceled?: KeyPressTo[];
+    };
+
+    conditions?: Condition[];
+}
+
+export interface HyperKeyConfig {
+    id: string;
+    description: string;
+    from: KeyPressFrom;
+    to_if_alone?: KeyPressTo[];
+}
+
+export interface Rule {
+    description: string;
+    manipulators: Manipulator[];
+}
+
+export interface KarabinerConfig {
+    global?: any;
+    profiles?: {
+        name?: string;
+        complex_modifications?: {
+            parameters?: any;
+            rules?: Rule[];
+        };
+    }[];
+}
+
+export class KarabinerComplexModifications {
+    rules = [] as Rule[];
+    title = "Deno Karabiner";
+    id = "deno";
+
+    constructor(options?: { title?: string; id?: string }) {
+        if (options?.title) {
+            this.title = options.title;
+        }
+        if (options?.id) {
+            this.id = options.id;
+        }
+    }
+
+    addRule(rule: Rule | Rule[]) {
+        if (Array.isArray(rule)) {
+            this.rules.push(...rule);
+        } else {
+            this.rules.push(rule);
+        }
+    }
+
+    getRules(): Rule[] {
+        const rules: Rule[] = [];
+
+        for (const rule of this.rules) {
+            if (rule instanceof HyperKey) {
+                rules.push(rule.getHyperKeyRule());
+                rules.push(...rule.getKeyBindingRules());
+            } else {
+                rules.push(rule);
+            }
+        }
+
+        return rules;
+    }
+
+    /**
+     * In the format of
+     * https://karabiner-elements.pqrs.org/docs/json/root-data-structure/#custom-json-file-in-configkarabinerassetscomplex_modifications
+     */
+    print() {
+        console.log(
+            JSON.stringify(
+                { title: this.title, rules: this.getRules() },
+                null,
+                "    ",
+            ),
+        );
+    }
+
+    async writeToProfile(profileName: string, configPath?: string) {
+        if (!configPath) {
+            const homeDir = Deno.env.get("HOME");
+            configPath = homeDir + "/.config/karabiner/karabiner.json";
+        }
+
+        const content = await Deno.readTextFile(configPath);
+
+        const config: KarabinerConfig | undefined = JSON.parse(content);
+
+        const profile = config?.profiles?.find((profile) => {
+            return profile.name === profileName;
+        });
+
+        const availableProfiles =
+            config?.profiles
+                ?.map((profile) => {
+                    return `"${profile.name}"`;
+                })
+                .join(", ") ?? "No profiles available";
+
+        if (!profile) {
+            throw new Error(
+                `Could not find Karabiner profile profile "${profileName}". Available profiles: ${availableProfiles}`,
+            );
+        }
+
+        if (!profile.complex_modifications) {
+            profile.complex_modifications = {};
+        }
+
+        profile.complex_modifications.rules = this.getRules();
+
+        await Deno.writeTextFile(
+            configPath,
+            JSON.stringify(config, null, "  "),
+        );
+    }
+}
+
+export interface HyperKeyBinding {
+    /**
+     * Description for Karabiner
+     */
+    description: string;
+    /**
+     * Symbol for the image generator
+     */
+    symbol?: string;
+    /**
+     * Key to bind the hyper key to
+     */
+    key: Key;
+
+    /**
+     * Emit these
+     */
+    to: KeyPressTo | KeyPressTo[];
+
+    conditions?: Condition[];
+
+    /**
+     * Allow duplicate defition for the same key. Set to true if you want
+     * different behaviour using the conditions
+     */
+    allowDuplicate?: boolean;
+}
+
+/**
+ * https://karabiner-elements.pqrs.org/docs/json/extra/virtual-modifier/
+ */
+export class VirtualModifier {
+    id: string;
+    from: KeyPressFrom;
+
+    constructor(id: string, from: KeyPressFrom) {
+        this.id = id;
+        this.from = from;
+    }
+
+    getManipulator(extensions: Partial<Manipulator>): Manipulator {
+        return {
+            ...extensions,
+            type: "basic",
+            from: this.from,
+            to: [
+                {
+                    set_variable: {
+                        name: this.id,
+                        value: 1,
+                    },
+                },
+                ...(extensions.to ?? []),
+            ],
+            to_after_key_up: [
+                {
+                    set_variable: {
+                        name: this.id,
+                        value: 0,
+                    },
+                },
+                ...(extensions.to_after_key_up ?? []),
+            ],
+        };
+    }
+
+    getCondition(): VariableCondition {
+        return {
+            type: "variable_if",
+            name: this.id,
+            value: 1,
+        };
+    }
+}
+
+export class HyperKey {
+    config: HyperKeyConfig;
+    bindings: HyperKeyBinding[];
+    virtualModifier: VirtualModifier;
+    id: string;
+
+    constructor(config: HyperKeyConfig) {
+        this.config = config;
+        this.id = config.id;
+        this.virtualModifier = new VirtualModifier(this.id, config.from);
+        this.bindings = [];
+    }
+
+    bindKey(newBinding: HyperKeyBinding) {
+        let existing;
+
+        if (!newBinding.allowDuplicate) {
+            existing = this.bindings.find((bin) => {
+                return newBinding.key === bin.key;
+            });
+        }
+
+        if (existing) {
+            throw new Error(
+                `Cannot bind "${newBinding.description}" "${newBinding.key}" is already defined for hyper key "${this.config.description}" with "${existing.description}"`,
+            );
+        }
+
+        this.bindings.push(newBinding);
+
+        return {
+            binding: newBinding,
+            alt: this.bindAlt.bind(this, newBinding),
+        };
+    }
+
+    bindAlt(
+        existing: HyperKeyBinding,
+        options: AltCondition,
+        alt: Partial<HyperKeyBinding>,
+    ) {
+        const existingConditions = [...(existing.conditions ?? [])];
+
+        if (existing.conditions) {
+            existing.conditions.push(...options.disable);
+        } else {
+            existing.conditions = options.disable;
+        }
+
+        const newBinding: HyperKeyBinding = {
+            ...existing,
+            ...alt,
+            conditions: [...existingConditions, ...options.enable],
+        };
+
+        this.bindings.push(newBinding);
+    }
+
+    getBindings() {
+        return this.bindings;
+    }
+
+    getHyperKeyRule(): Rule {
+        return {
+            description: `${this.config.id}: "${this.config.description}"`,
+            manipulators: [
+                this.virtualModifier.getManipulator({
+                    to_if_alone: this.config.to_if_alone,
+                }),
+            ],
+        };
+    }
+
+    getKeyBindingRules(): Rule[] {
+        return this.bindings.map((bin) => {
+            const customConditions = bin.conditions || [];
+
+            return {
+                description: `${this.id}: "${bin.key}" to ${bin.description}`,
+                manipulators: [
+                    {
+                        type: "basic",
+                        from: {
+                            key_code: bin.key,
+                            modifiers: this.config.from.modifiers,
+                        },
+                        conditions: [
+                            this.virtualModifier.getCondition(),
+                            ...customConditions,
+                        ],
+                        to: [bin.to].flat(),
+                    },
+                ],
+            };
+        });
+    }
+
+    getRules() {
+        return [this.getHyperKeyRule(), ...this.getKeyBindingRules()];
+    }
+}
